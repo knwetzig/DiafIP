05b8f021ead5067355a6040b0a595dc9623b5f2d
knwetzig <knwetzig@c61c2f73-aa0a-e4e6-a283-4d5e3ed0e483>
2014-06-25 14:44:54 +0000

    - Die Daten werden jetzt nicht mehr direkt einzeln in Eigenschaften gespeichert,   sondern in einem Array abgelegt. Änderungen in class.entity bereits vollzogen.   class.namen & class.person folgen - ID's werden nun hexadezimal angezeigt. Intern  weiterhin im Dezimalsystem. - BAUSTELLE: name.add()

--- inc/class.entity.php
+++ inc/class.entity.php
@@ -35,17 +35,32 @@ abstract class entity implements iEntity {
         GETDATA = 'SELECT * FROM entity WHERE id = ?;';
 
     protected
-        $id         = null,
-        $bereich    = '',       // Enthält die Kennung zu welchem Bereich die
-                                // Entität gehört....
-        $descr      = '',       // Beschreibung bzw. Biografie bei Personen
-        $bilder     = array(),
-        $notiz      = '',       // selbsterklärend ;-)
-        $isvalid    = false,    // Flag zur Kennzeichnung, das dieser Datensatz
-                                // abschließend bearbeitet wurde
-        $del        = false,    // Löschflag
-        $editfrom   = null,     // uid des Bearbeiters
-        $editdate   = null;     // timestamp width Timezone
+        $content = array(
+            'id'         => null,
+            'bereich'    => '',      // Enthält die Kennung zu welchem Bereich die
+                                     // Entität gehört....
+            'descr'      => '',      // Beschreibung bzw. Biografie bei Personen
+            'bilder'     => array(),
+            'notiz'      => '',      // selbsterklärend ;-)
+            'isvalid'    => false,   // Flag zur Kennzeichnung, das dieser Datensatz
+                                     // abschließend bearbeitet wurde
+            'del'        => false,   // Löschflag
+            'editfrom'   => null,    // uid des Bearbeiters
+            'editdate'   => null     // timestamp width Timezone
+        );
+
+    private
+        $types = array(
+        'integer',
+        'text',     // bereich
+        'text',     // descr
+        'text',     // bilder (eigtl ein array)
+        'text',     // notiz
+        'boolean',  // isvalid
+        'boolean',  // del
+        'integer',  // uid
+        'date'      // editdate (soll nicht konvertiert werden!)
+    );
 
     function __construct($nr = null) {
             if(isset($nr) AND is_numeric($nr)) self::get(intval($nr));
@@ -53,25 +68,14 @@ abstract class entity implements iEntity {
 
     protected function get($nr) {
     // Diese Funktion initialisiert das Objekt
-        $types = array(
-            'integer',
-            'text',     // bereich
-            'text',     // descr
-            'text',     // bilder (eigtl ein array)
-            'text',     // notiz
-            'boolean',  // isvalid
-            'boolean',  // del
-            'integer',  // uid
-            'date'      // editdate (soll nicht konvertiert werden!)
-        );
 
         $db =& MDB2::singleton();
-        $data = $db->extended->getRow(self::GETDATA,$types,$nr,'integer');
+        $data = $db->extended->getRow(self::GETDATA,$this->types,$nr,'integer');
         IsDbError($data);
         if($data) :
-            foreach($data as $key => $val) $this->$key = $val;
-            if($this->bilder) :
-                $this->bilder = preg_split("/[,{}]/", $this-> bilder, null, PREG_SPLIT_NO_EMPTY);
+            foreach($data as $key => $val) $this->content[$key] = $val;
+            if($this->content['bilder']) :
+                $this->content['bilder'] = preg_split("/[,{}]/", $this->content['bilder'], null, PREG_SPLIT_NO_EMPTY);
                 // Achtung Elemente liegen als Text vor! (nicht integer)
             endif;
         else :
@@ -91,9 +95,9 @@ abstract class entity implements iEntity {
      **********************************************************/
         $db =& MDB2::singleton();
         $bearbeiter = null;
-        if(!empty($this->editfrom)) :
+        if(!empty($this->content['editfrom'])) :
             $bearbeiter = $db->extended->getCol(
-                'SELECT realname FROM s_auth WHERE uid = '.$this->editfrom.';');
+                'SELECT realname FROM s_auth WHERE uid = '.$this->content['editfrom'].';');
             IsDbError($bearbeiter);
         endif;
         if($bearbeiter) return $bearbeiter[0];
@@ -105,8 +109,8 @@ abstract class entity implements iEntity {
      *  Return: none
      **********************************************************/
         global $myauth;
-            $this->editfrom = $myauth->getAuthData('uid');
-            $this->editdate = $_SERVER['REQUEST_TIME'];
+            $this->content['editfrom'] = $myauth->getAuthData('uid');
+            $this->content['editdate'] = $_SERVER['REQUEST_TIME'];
     }
 
     function setValid() {
@@ -114,7 +118,7 @@ abstract class entity implements iEntity {
      * Aufgabe: Bearbeitungsflag setzen (Kippschalter)
      *  Return: none
      **********************************************************/
-        if($this->isValid) $this->isValid = false; else $this->isValid = true;
+        if($this->content['isValid']) $this->content['isValid'] = false; else $this->content['isValid'] = true;
     }
 
     function setDel() {
@@ -122,17 +126,17 @@ abstract class entity implements iEntity {
      * Aufgabe: Löschflag setzen (Kipschalter)
      *  Return: none
      **********************************************************/
-        if($this->del) $this->del = false; else $this->del = true;
+        if($this->content['del']) $this->content['del'] = false; else $this->content['del'] = true;
     }
 
     function lview() {
         $data = array(
         // name,inhalt optional-> $rechte,$label,$tooltip,valString
-            new d_feld('id',     $this->id,       VIEW),
-            new d_feld('bereich', $this->bereich, VIEW),
-            new d_feld('bilder', $this->bilder, VIEW),
-            new d_feld('edit',   null,          EDIT, null, 4013), // edit-Button
-            new d_feld('del',    null,          DELE, null, 4020), // Lösch-Button
+            new d_feld('id', dez2hex($this->content['id']), VIEW),
+            new d_feld('bereich',$this->content['bereich'], VIEW),
+            new d_feld('bilder', $this->content['bilder'], VIEW),
+            new d_feld('edit', null, EDIT, null, 4013), // edit-Button
+            new d_feld('del', null, DELE, null, 4020), // Lösch-Button
         );
         return $data;
     }
@@ -140,13 +144,13 @@ abstract class entity implements iEntity {
     function view() {
         $data = array(
         // name,inhalt optional-> $rechte,$label,$tooltip,valString
-            new d_feld('descr',  changetext($this->descr),  VIEW,   513),   // Beschreibung
-            new d_feld('notiz',  changetext($this->notiz),  IVIEW,  514),   // Notiz
+            new d_feld('descr',  changetext($this->content['descr']), VIEW,513), // Beschreibung
+            new d_feld('notiz',  changetext($this->content['notiz']), IVIEW, 514),
             // Bearbeitungssymbole und -ausgaben
 
-            new d_feld('chdatum', $this->editdate, EDIT),
+            new d_feld('chdatum', $this->content['editdate'], EDIT),
             new d_feld('chname', $this->getBearbeiter(), EDIT),
-            new d_feld('isvalid', $this->isvalid, IVIEW)
+            new d_feld('isvalid', $this->content['isvalid'], IVIEW)
         );
         return array_merge(self::lview(), $data);
     }
