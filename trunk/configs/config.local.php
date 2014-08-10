5fcfe2b01a8a7d10207c83509f10ae70f7c571cd
knwetzig <knwetzig@c61c2f73-aa0a-e4e6-a283-4d5e3ed0e483>
2014-08-10 14:25:36 +0000

    aktualisierung Arbeitskopie

--- trunk/configs/config.local.php
+++ trunk/configs/config.local.php
@@ -10,18 +10,18 @@ $URL$
 
 ***** (c) DIAF e.V. *******************************************/
 
-error_reporting(E_ALL & ~E_DEPRECATED);
+error_reporting(E_ALL & ~E_DEPRECATED & ~E_STRICT);
 set_include_path('/pear/lib:/smarty/libs:inc');
 date_default_timezone_set('Europe/Berlin');
 
 /* Die DSN steht in einer separaten Datei ausserhalb DocumentRoot */
-require_once    '../../conf/dsn';
+require_once    '/conf/dsn';
 
 $smartyConf = array(
     'compile_dir'     => '/tmp',          // '/tmp';
     'config_dir'      => 'configs',       // Verzeichnis der Kongurationsdateien
     'cache_dir'       => '/tmp',
-    'force_compile'   =>  false,
-    'debugging'       =>  true           // (true | false)
+    'force_compile'   =>  true,
+    'debugging'       =>  false           // (true | false)
 );
 ?>
